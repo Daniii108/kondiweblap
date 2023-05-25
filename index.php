73a515e40acd6efc1025e9b57e95582445438cc1
Q1tuzu123 <126662836+Q1tuzu123@users.noreply.github.com>
2023-05-25 23:31:28 +0200

    new weblap

--- index.php
+++ index.php
@@ -1,117 +1,314 @@
-<!doctype html>
+<?php
+$db_pass="n3RYM!_36b1eIQm9";
+$db_name="gymfun";
+?>
+
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  </head>
-  <body>
-  <?php include_once('navigation.php') ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FunGym</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="/23/style.css">
+</head>
+<body>
+    
+<!-- header section starts      -->
 
-    <img  width="100%" src="/exam/gym.jpg" alt="">
-    <?php include_once('navigation.php') ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <!-- Footer -->
-<footer class="text-center text-lg-start bg-light text-muted">
-    <!-- Right -->
-    <div>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-facebook-f"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-twitter"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-google"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-instagram"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-linkedin"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-github"></i>
-      </a>
+<header class="header">
+
+    <a href="#" class="logo"> <span>FUN</span>GYM</a>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+    <nav class="navbar">
+        <a href="#about">about</a>
+        <a href="#features">features</a>
+        <a href="#pricing">pricing</a>
+        <a href="#trainers">classes</a>
+        <a href="#blogs">SignUp</a>
+    </nav>
+
+</header>
+
+<!-- header section ends     -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="swiper home-slider">
+
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slide slide" style="background: url(img/gym_icon.jpg) no-repeat;">
+                <div class="content">
+                    <h3>Be strong</h3>
+                    <h3>Be fit, have fun.</h3>
+
+                </div>
+            </div>
+        </div>
+        <div class="swiper-pagination"></div>
     </div>
-    <!-- Right -->
-  </section>
-  <!-- Section: Social media -->
 
-  <!-- Section: Links  -->
-  <section class="">
-    <div class="container text-center text-md-start mt-5">
-      <!-- Grid row -->
-      <div class="row mt-3">
-        <!-- Grid column -->
-        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-          <!-- Content -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3"></i>GYM
-          </h6>
-          <p>
-          A good gym is a facility that promotes physical activity, provides a safe, functional, and comfortable workout environment, and creates a pleasant and enjoyable atmosphere for recreation and socialization.
-          </p>
-        </div>
-        <!-- Grid column -->
+</section>
 
-        <!-- Grid column -->
-        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            Courses
-          </h6>
-          <p>
-            <a href="/exam/courses.php" class="text-reset">Spinning</a>
-          </p>
-          <p>
-            <a href="/exam/courses.php" class="text-reset">Yoga</a>
-          </p>
-          <p>
-            <a href="/exam/courses.php" class="text-reset">Boxing</a>
-          </p>
-          
-        </div>
-        <!-- Grid column -->
+<!-- home section ends -->
 
-        <!-- Grid column -->
-        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            Useful links
-          </h6>
-          <p>
-            <a href="/exam/pricing.php" class="text-reset">Pricing</a>
-          </p>
-          <p>
-            <a href="/exam/index.php" class="text-reset">Settings</a>
-          </p>
-          <p>
-            <a href="/exam/index.php" class="text-reset">Help</a>
-          </p>
-        </div>
-        <!-- Grid column -->
+<!-- about section starts  -->
 
-        <!-- Grid column -->
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
-          <p>
-            <i class="fas fa-envelope me-3"></i>
-            info@example.com
-          </p>
-          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-        </div>
-        <!-- Grid column -->
-      </div>
-      <!-- Grid row -->
+<section class="about" id="about">
+
+    <div class="image">
+        <img src="img/gym.jpg" alt="">
     </div>
-  </section>
-  <!-- Section: Links  -->
 
-</footer>
-<!-- Footer -->
-  </body>
+    <div class="content">
+        <span>about us</span>
+        <h3 class="title">Just have fun.</h3>
+        <div class="box-container">
+            <div class="box">
+                <h3><i class="fas fa-check"></i>Fun</h3>
+                <p>High variety of programs</p>
+            </div>
+            <div class="box">
+                <h3><i class="fas fa-check"></i>healthy life</h3>
+                <p>Diet plans and personal training.</p>
+            </div>
+            <div class="box">
+                <h3><i class="fas fa-check"></i>classes</h3>
+                <p>More and More classes to come, for everybody to enjoy.</p>
+            </div>
+            <div class="box">
+                <h3><i class="fas fa-check"></i>workout</h3>
+                <p>Well equiped gym for averybody, and you can hav help from our professional trainers.</p>
+            </div>
+        </div>
+        <a href="#" class="btn">read more</a>
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- features section starts  -->
+
+<section class="features" id="features">
+
+    <h1 class="heading"> <span>gym features</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="img/weight3.avif" alt="">
+            </div>
+            <div class="content">
+                <img src="img/weight_icon.jpeg" alt="">
+                <h3>weight lifting</h3>
+                <p></p>
+                <a href="#" class="btn">read more</a>
+            </div>
+        </div>
+
+        <div class="box second">
+            <div class="image">
+                <img src="img/diet2.jpg" alt="">
+            </div>
+            <div class="content">
+                <img src="img/diet_icon.png" alt="">
+                <h3>diet plan</h3>
+                <p></p>
+                <a href="#" class="btn">read more</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="img/personal3.avif" alt="">
+            </div>
+            <div class="content">
+                <img src="img/personal icon.jpeg" alt="">
+                <h3>personal training</h3>
+                <p></p>
+                <a href="#" class="btn">read more</a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- features ends -->
+
+<!-- pricing starts  -->
+
+<section class="pricing" id="pricing">
+
+    <div class="information">
+        <span id="pricingplan">pricing plan</span>
+        <h3>affordable pricing plan for your</h3>
+        <p> <i class="fas fa-check"></i> Yoga </p>
+        <p> <i class="fas fa-check"></i> Spining </p>
+        <p> <i class="fas fa-check"></i> Boxing </p>
+        <p> <i class="fas fa-check"></i> Gym </p>
+        <a href="#" class="btn">all pricing</a>
+    </div>
+
+    <div class="plan basic">
+        <h3>basic plan</h3>
+        <div class="price"><span>$</span>30<span>/month</span></div>
+       <div class="list">
+        <p> <i class="fas fa-check"></i> weight lifting </p>
+        <p> <i class="fas fa-check"></i> diet plans </p>
+        <p> <i class="fas fa-times"></i> </p>
+        <p> <i class="fas fa-times"></i></p>
+       </div>
+       <a href="#" class="btn">get started</a>
+    </div>
+
+    <div class="plan">
+        <h3>premium plan</h3>
+        <div class="price"><span>$</span>90<span>/month</span></div>
+       <div class="list">
+        <p> <i class="fas fa-check"></i> weight lifting </p>
+        <p> <i class="fas fa-check"></i> diet plans </p>
+        <p> <i class="fas fa-check"></i> personal training </p>
+        <p> <i class="fas fa-check"></i> classes </p>
+       </div>
+       <a href="#" class="btn">get started</a>
+    </div>
+
+</section>
+
+<!-- pricing section ends -->
+
+<!-- Clasess section starts  -->
+
+<section class="trainers" id="trainers">
+
+    <h1 class="heading"> <span>Classes</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="img/yoga2.jpeg" alt="">
+            <div class="content">
+                <h3>Yoga</h3>
+                <div class="share">
+                    <span>Yoga is a practice that connects the body, breath, and mind. It uses breathing exercises, and meditation to improve overall health.</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="img/boxing.jpg" alt="">
+            <div class="content">
+                <h3>Boxing</h3>
+                <div class="share">
+                    <span>
+                        Fitness boxing has adapted boxing movements into exercise routines that bring many physical and mental health benefits.
+                    </span>
+                </div>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="img/spinning.jpg" alt="">
+            <div class="content">
+                <h3>Spining</h3>
+                <div class="share">
+                    <span>Indoor cycling, often called spinning, is a form of exercise with classes focusing on endurance and strength.</span>  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<!-- Clasess section ends -->
+
+<!-- banner section starts  -->
+
+<section class="banner">
+
+    <span>See the pricing</span>
+    <h3>Find the best pricing plan for you</h3>
+    <a href="#pricing" class="btn">pricing</a>
+
+</section>
+
+<!-- banner section ends -->
+
+<!-- Login section starts  -->
+
+<section class="blogs" id="blogs">
+
+    <h1 class="heading"> <span>Login</span> </h1>
+
+    <div class="swiper blogs-slider">
+
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slide slide">
+                <div class="image">
+                    <img src="img/login.jpeg" alt="">
+                </div>                    
+                <a href="/23/login.php" class="btn" id="sing">SignUp</a>
+
+        </div>
+
+    </div>
+
+</section>
+
+<!-- Login section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a class="links" href="#about">about</a>
+            <a class="links" href="#features">features</a>
+            <a class="links" href="#pricing">pricing</a>
+            <a class="links" href="#classes">Classes</a>
+            <a class="links" href="#Login">SingUp</a>
+        </div>
+
+        <div class="box">
+            <h3>opening hours</h3>
+            <p> monday : <i> 7:00am - 10:30pm </i> </p>
+            <p> tuesday : <i> 7:00am - 10:30pm </i> </p>
+            <p> wednesday : <i> 7:00am - 10:30pm </i> </p>
+            <p> friday : <i> 7:00am - 10:30pm </i> </p>
+            <p> saturday : <i> 7:00am - 10:30pm </i> </p>
+            <p> sunday : <i> closed </i> </p>
+        </div>
+
+        <div class="box">
+            <h3>opening hours</h3>
+            <p> <i class="fas fa-phone"></i> +3620436745584</p>
+            <p> <i class="fas fa-envelope"></i> GymFun@gmail.com </p>
+            <p> <i class="fas fa-map"></i> 2632 Foley Street,Florida </p>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+                <a href="#" class="fab fa-pinterest"></a>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<!-- footer section ends -->
+</body>
 </html>
